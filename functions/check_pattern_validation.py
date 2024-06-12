2a938baa2b204b5b89d50b193fd97269d830649c
tikesanoo <softguide.tikesanoo@gmail.com>
2024-06-12 12:06:07 +0630

    validation

--- functions/check_pattern_validation.py
+++ functions/check_pattern_validation.py
@@ -1,10 +1,17 @@
 import re
 
-def has_aliases(sql_query):
-    # Extract the part of the query after the SELECT and before the FROM
-    select_clause = re.search(r"SELECT(.*?)FROM", sql_query, re.IGNORECASE | re.DOTALL)
-    if select_clause:
-        select_clause_text = select_clause.group(1)
-        # Check if there's any "AS" in the select clause, indicating an alias
-        return bool(re.search(r"\bAS\b", select_clause_text, re.IGNORECASE))
-    return False
+def has_select_query(query):
+    select_pattern = r'^\s*SELECT\b'
+    return bool(re.match(select_pattern, query, re.IGNORECASE))
+
+def has_insert_query(query):
+    insert_pattern = r'^\s*INSERT\s+INTO\b'
+    return bool(re.match(insert_pattern, query, re.IGNORECASE))
+
+def has_update_query(query):
+    update_pattern = r'^\s*UPDATE\b'
+    return bool(re.match(update_pattern, query, re.IGNORECASE))
+
+def has_delete_query(query):
+    delete_pattern = r'^\s*DELETE\s+FROM\b'
+    return bool(re.match(delete_pattern, query, re.IGNORECASE))
